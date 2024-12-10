e8ece165c00249d0af38fd97c56da91ad7d3cf6e
Madhusudan N <madhu1992blue@outlook.com>
2024-12-10 04:34:02 +0000

    add support for extracting markdown components

---
+++ src/test_extract_text_components.py
@@ -0,0 +1,8 @@
+import unittest
+
+from extract_text_components import extract_markdown_images, extract_markdown_text
+
+class TestExtractTextComponents(unittest.TestCase):
+    def test_img_extraction(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(extract_markdown_images(text),[("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
