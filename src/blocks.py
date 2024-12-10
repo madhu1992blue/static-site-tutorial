8ab0120055cbff9d49a3fb0ec976ce031110b82e
Madhusudan N <madhu1992blue@outlook.com>
2024-12-10 15:12:45 +0000

    handle unnecessary lines between blocks

--- src/blocks.py
+++ src/blocks.py
@@ -3,6 +3,8 @@ def markdown_to_blocks(markdown):
     intermediate_lines = []
     for line in markdown.split("\n"):
         if line == "":
+            if len(intermediate_lines) == 0:
+                continue
             blocks.append("\n".join(intermediate_lines))
             intermediate_lines = []
         else:
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -22,3 +22,30 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         self.assertEqual(blocks[0], heading)
         self.assertEqual(blocks[1], paragraph)
         self.assertEqual(blocks[2], markdown_list)
+
+
+    def test_extra_lines_between_blocks(self):
+
+        markdown = \
+"""# This is a heading
+
+
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+"""
+        heading = "# This is a heading"
+        paragraph = "This is a paragraph of text. It has some **bold** and *italic* words inside of it."
+        markdown_list = \
+"""* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+        blocks = markdown_to_blocks(markdown)
+        self.assertEqual(blocks[0], heading)
+        self.assertEqual(blocks[1], paragraph)
+        self.assertEqual(blocks[2], markdown_list)
